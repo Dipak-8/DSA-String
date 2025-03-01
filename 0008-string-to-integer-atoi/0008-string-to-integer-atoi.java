d44ef23acc423d440f3e6facb1700f0b6e753c9c
Dipak Das <dipakdas84630@gmail.com>
2025-03-01 14:19:58 +0530

    Time: 1 ms (100%), Space: 42.2 MB (95.85%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -5,25 +5,29 @@ class Solution {
         int n = s.length();
         long digit = 0;
 
-        if(n == 0)
+        // if length is 0 
+        if(n == 0)  
             return 0;
 
-        while(i < n && s.charAt(i) == ' ')
+        // ignoring the white spaces at first
+        while(i < n && s.charAt(i) == ' ') // 
             i++;
-        
+
+        // determining the sign 
         if(i < n && (s.charAt(i) == '-' || s.charAt(i) == '+')) {
             sign = (s.charAt(i) == '-') ? -1 : 1;
             i++;
         }
 
+        // finding the digit
         while(i < n && (s.charAt(i) >= 48 && s.charAt(i) <= 57)) {
             digit = digit * 10 + (s.charAt(i) - '0');
 
+            // controlling the overflow
             if(digit * sign > Integer.MAX_VALUE) return Integer.MAX_VALUE;
             if(digit * sign < Integer.MIN_VALUE) return Integer.MIN_VALUE;
             i++;
         }
-
         return (int)digit * sign;
     }
 }
